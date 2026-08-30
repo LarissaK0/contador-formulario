7d031c9719fd6741be5ee61fc9fd245cef513839
Larissa <larissak0@hotmail.com>
2026-08-30 18:44:46 -0300

    config github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/contador-formulario/',
   server: {
     port: 3000
   }
